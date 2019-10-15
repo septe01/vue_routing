0d5a36d4d798d6c973054c46756a81a7b87a27b5
Septe Habudin <septe.1991@gmail.com>
2019-10-15 20:17:37 +0700

    Vue Routeng

---
+++ src/views/blog/list.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="blog" id="blog">
+    <ul>
+        <li v-for="blog in blogs" :key="blog.id">
+            {{blog.name}}
+        </li>
+    </ul>
+    
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+export default {
+    name:'blog',
+  data () {
+    return {
+      yourname: "septe",
+      blogs: []
+    };
+  },
+  async created() {
+      try{
+          const res = await axios.get(`http://nasution.dev/api/blog`);
+          this.blogs = res.data
+      }catch( e ){
+          console.log(e)
+      }
+  },
+}
+</script>
---
+++ src/views/task/list.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="task" >
+    <h1>This is an task page</h1>
+    <ul>
+        <li v-for="task in Tasks" :key="task.id">
+            {{task.name}}
+        </li>
+    </ul>
+  </div>
+</template>
+
+
+
+<script>
+import axios from 'axios'
+export default {
+    name:'task',
+  data () {
+    return {
+      yourname: "septe",
+      Tasks: []
+    };
+  },
+  async created() {
+      try{
+          const res = await axios.get(`http://nasution.dev/api/tasks`);
+          this.Tasks = res.data
+      }catch( e ){
+          console.log(e)
+      }
+  },
+}
+</script>
