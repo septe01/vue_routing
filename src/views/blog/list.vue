527aa4e5e7792256c8307cefd090846b6a1c8f4d
vhirgo <septe.1991@gmail.com>
2019-10-16 13:25:34 +0700

    blog n task

--- src/views/blog/list.vue
+++ src/views/blog/list.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="blog" id="blog">
-    <ul>
-        <li v-for="blog in blogs" :key="blog.id">
-            {{blog.name}}
-        </li>
-    </ul>
-    
+    <div class="container">
+      <div class="row justify-content-center">
+        <h1>This is an Blog </h1>
+      </div>
+      <div class="row justify-content-center">
+          <div class="col-md-3">
+                  Title
+          </div>
+          <div class="col-md-3">
+                  Content
+          </div>
+      </div>
+      <div v-for="blog in blogs" :key="blog" >
+        <div class="row justify-content-center" v-for="blg in blog" :key="blg">
+          
+          <div class="col-md-3 text-left">
+              {{blg.title}}
+          </div>
+          <div class="col-md-3 text-left">
+              {{blg.content}}
+          </div>
+        </div>
+        
+      </div>
+    </div>  
   </div>
 </template>
 
--- src/views/task/list.vue
+++ src/views/task/list.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="task" >
-    <h1>This is an task page</h1>
-    <ul>
-        <li v-for="task in Tasks" :key="task.id">
-            {{task.name}}
-        </li>
-    </ul>
+    
+    <div class="container">
+      <div class="row justify-content-center">
+        <h1>This is an task </h1>
+      </div>
+      <div class="row justify-content-center">
+          <div class="col-md-">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nama</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr  v-for="(task, index) in Tasks" :key="task.id">
+                  <td >{{index + 1}}</td>
+                  <td class="text-left">{{task.name}}</td>
+                  <td class="text-left">{{task.description}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+      </div>
+    </div>  
   </div>
 </template>
 
