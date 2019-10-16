6de4fcfed9b0aa8e08e74c3888d2de2008e1cee7
vhirgo <septe.1991@gmail.com>
2019-10-16 15:59:23 +0700

    Perbaikan tampilan blog

--- src/views/blog/list.vue
+++ src/views/blog/list.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="blog" id="blog">
+     
     <div class="container">
       <div class="row justify-content-center">
         <h1>This is an Blog </h1>
       </div>
       <div class="row justify-content-center">
-          <div class="col-md-3">
-                  Title
+          <div class="col-md-">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Title</th>
+                  <th>Content</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr  v-for="(blog, index) in blogs" :key="blog.id">
+                  <td >{{index + 1}}</td>
+                  <td class="text-left">{{blog.title}}</td>
+                  <td class="text-left">{{blog.content}}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <div class="col-md-3">
-                  Content
-          </div>
-      </div>
-      <div v-for="blog in blogs" :key="blog" >
-        <div class="row justify-content-center" v-for="blg in blog" :key="blg">
-          
-          <div class="col-md-3 text-left">
-              {{blg.title}}
-          </div>
-          <div class="col-md-3 text-left">
-              {{blg.content}}
-          </div>
-        </div>
-        
       </div>
     </div>  
+
   </div>
 </template>
 
@@ -42,7 +44,7 @@ export default {
   async created() {
       try{
           const res = await axios.get(`http://nasution.dev/api/blog`);
-          this.blogs = res.data
+          this.blogs = res.data.data
       }catch( e ){
           console.log(e)
       }
